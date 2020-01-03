df8ae5e5618b827c4a5d812f74371d1417228b2a
Lawrence-Tang <tangzongsheng@gmail.com>
2020-01-03 12:52:15 +0800

    added support for r19-snapshot

--- s5p4418/smart4418.mk
+++ s5p4418/smart4418.mk
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. device/friendlyelec/s5p4418/nanopi_fire2a.mk
+. device/friendlyelec/s5p4418/base.mk
 TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_Smart4418_armhf_sd.img
 TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_Smart4418_armhf_eflasher.img
 
