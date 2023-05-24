cb9076bed5babf74a07b597c1749406269cec7ad
nbnbnb <nbnbnb.zhang@gmail.com>
2023-05-24 12:35:28 +0800

    Update nanopi_m2a.mk

--- s5p4418/nanopi_m2a.mk
+++ s5p4418/nanopi_m2a.mk
@@ -4,4 +4,5 @@
 TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-M2A_armhf_sd.img
 TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-M2A_armhf_eflasher.img
 
+FRIENDLYWRT_PACKAGE_DIR=bin/targets/nexell-s5p4418/generic/packages
 
